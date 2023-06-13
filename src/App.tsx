0cd4fa86b448077a888a3779098ebd9e02547047
Ormide <pierre.ledoux@outlook.fr>
2023-06-13 19:06:06 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1>Hello World</h1>
+    <p>Ceci est le début de l'application React pour le projet back office de VillageGreen</p>
+    </>
+  )
+}
+
+export default App
