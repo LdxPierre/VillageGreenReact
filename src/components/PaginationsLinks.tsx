8ae80a7c93e9f6080d62563cbc18a490af18cceb
Pierre <pierre.ledoux@outlook.fr>
2023-06-27 14:13:33 +0200

    productsList

--- src/components/PaginationsLinks.tsx
+++ src/components/PaginationsLinks.tsx
@@ -3,29 +3,29 @@ import PaginationInterface from "../types/PaginationInterface";
 import CategoryInterface from "../types/CategoryInterface";
 
 interface Props {
-  pagination: PaginationInterface;
-  getProducts: (category?: CategoryInterface, filters?: string) => void;
-  page: number;
-  setPage: (page: number) => void;
+	pagination: PaginationInterface;
+	getProducts: (category?: CategoryInterface, filters?: string) => void;
+	page: number;
+	setPage: (page: number) => void;
 }
 
 const PaginationLinks = ({ pagination, getProducts, page, setPage }: Props): JSX.Element => {
-  const lastPage = Number(pagination["hydra:last"].charAt(pagination["hydra:last"].length - 1));
+	const lastPage = Number(pagination["hydra:last"].charAt(pagination["hydra:last"].length - 1));
 
-  const handleChangePage = (event: React.ChangeEvent<unknown>, value: number) => {
-    if (value != page) {
-      const IRI = new URL(`http://localhost:8000/${pagination["@id"]}`);
-      IRI.searchParams.set("page", String(value));
-      setPage(value);
-      getProducts(undefined, IRI.searchParams.toString());
-    }
-  };
+	const handleChangePage = (event: React.ChangeEvent<unknown>, value: number) => {
+		if (value != page) {
+			const IRI = new URL(`http://localhost:8000/${pagination["@id"]}`);
+			IRI.searchParams.set("page", String(value));
+			setPage(value);
+			getProducts(undefined, IRI.searchParams.toString());
+		}
+	};
 
-  return (
-    <Stack spacing={2}>
-      <Pagination count={lastPage} page={page} onChange={handleChangePage} />
-    </Stack>
-  );
+	return (
+		<Stack spacing={2} direction={"row"} justifyContent={"flex-end"} sx={{ paddingY: 1 }}>
+			<Pagination count={lastPage} page={page} onChange={handleChangePage} />
+		</Stack>
+	);
 };
 
 export default PaginationLinks;
--- src/pages/inventory/components/ProductsList.tsx
+++ src/pages/inventory/components/ProductsList.tsx
@@ -2,32 +2,68 @@ import PaginationInterface from "../../../types/PaginationInterface";
 import ProductInterface from "../../../types/ProductInterface";
 import PaginationLinks from "../../../components/PaginationsLinks";
 import CategoryInterface from "../../../types/CategoryInterface";
-import { CircularProgress } from "@mui/material";
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 
 interface Props {
-  products: ProductInterface[];
-  pagination: PaginationInterface | null;
-  page: number;
-  setPage: (page: number) => void;
-  getProducts: (category?: CategoryInterface, filters?: string) => void;
-  loading: boolean;
+	products: ProductInterface[];
+	pagination: PaginationInterface | null;
+	page: number;
+	setPage: (page: number) => void;
+	getProducts: (category?: CategoryInterface, filters?: string) => void;
+	loading: boolean;
 }
 
-const ProductsList = ({ products, pagination, getProducts, loading, page, setPage }: Props): JSX.Element => {
-  return (
-    <>
-      {pagination?.["hydra:last"] ? (
-        <PaginationLinks
-          pagination={pagination}
-          getProducts={getProducts}
-          page={page}
-          setPage={setPage}
-        ></PaginationLinks>
-      ) : null}
-      {loading ? <CircularProgress /> : null}
-      {products[0] && !loading ? products.map((e, i) => <p key={i}>{e.name}</p>) : null}
-    </>
-  );
+const ProductsList = ({ products, pagination, getProducts, page, setPage }: Props): JSX.Element => {
+	return (
+		<>
+			{pagination?.["hydra:last"] ? (
+				<PaginationLinks
+					pagination={pagination}
+					getProducts={getProducts}
+					page={page}
+					setPage={setPage}></PaginationLinks>
+			) : null}
+			<TableContainer component={Paper}>
+				<Table aria-label="Products">
+					<TableHead>
+						<TableRow>
+							<TableCell>ID</TableCell>
+							<TableCell>Nom</TableCell>
+							<TableCell align="right">Marque</TableCell>
+							<TableCell align="right">Stock</TableCell>
+							<TableCell align="right">Prix</TableCell>
+							<TableCell align="right">Action</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{products.map((p) => (
+							<TableRow key={p.id} hover>
+								<TableCell component={"th"} scope="row">
+									{p.id}
+								</TableCell>
+								<TableCell>{p.name}</TableCell>
+								<TableCell align="right">{p.brand}</TableCell>
+								<TableCell align="right">{p.stock}</TableCell>
+								<TableCell align="right">{p.price}</TableCell>
+								<TableCell align="right">
+									<Button size="small" variant="contained">
+										Afficher
+									</Button>
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
+			{pagination?.["hydra:last"] ? (
+				<PaginationLinks
+					pagination={pagination}
+					getProducts={getProducts}
+					page={page}
+					setPage={setPage}></PaginationLinks>
+			) : null}
+		</>
+	);
 };
 
 export default ProductsList;
