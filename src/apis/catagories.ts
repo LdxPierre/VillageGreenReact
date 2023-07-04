a8e811dbc20dc6858c574e8e0fc134076c831d4b
Pierre <pierre.ledoux@outlook.fr>
2023-07-04 11:18:11 +0200

    jwt token

--- src/apis/catagories.ts
+++ src/apis/catagories.ts
@@ -1,10 +1,16 @@
 const API_URL = "http://localhost:8000/api/categories";
+const ApiAuthToken =
+	"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2ODg0NjAwOTYsImV4cCI6MTY4ODQ2MzY5Niwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJuYW1lIjoidGVzdEB0ZXN0LmZyIn0.ep9CgKrq5ai1M84Bhk8Vh_ygOTsiz67EvnJroqrus9hPWCjRV_qesY1ZXnQI1JT8yuV1wO0aJfAKxW1VtmOmwq7lQyw0gMpxBImoVZgrUSaCYU8Iwln_0YqBVuI0FmtztkBKZ2LMCiz8yThXrConQdt1mKsRL00hn_6_a1erpjwvcUOWWF5hwhbYsGemHfGmdAh_Cx1K3K_M9iPVvkb3rvWaGmid9YxM3OFoint9Kg4fyVGTuQu7O4FpRM5HbvxZ65LG_zb69H_GbCWtr_McCVkzD95-4H5wZuvgtly1QWRy7xDV-1tXanaoqbEYuf1SKCnWbEYJW3WbTORqhnLaFQ";
 
 export const getCategories = async (params?: URLSearchParams) => {
-  const res: Response = await fetch(`${API_URL}${params ? `?${params}` : ""}`);
-  if (res.ok) {
-    return await res.json();
-  } else {
-    throw new Error("Error on getCategories");
-  }
+	const res: Response = await fetch(`${API_URL}${params ? `?${params}` : ""}`, {
+		headers: {
+			Authorization: ApiAuthToken,
+		},
+	});
+	if (res.ok) {
+		return await res.json();
+	} else {
+		throw new Error("Error on getCategories");
+	}
 };
--- src/apis/products.ts
+++ src/apis/products.ts
@@ -1,9 +1,28 @@
 import { HydraCollectionInterface } from "../types";
 
 const API_URL = "http://localhost:8000/api/products";
+const ApiAuthToken =
+	"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2ODg0NjAwOTYsImV4cCI6MTY4ODQ2MzY5Niwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJuYW1lIjoidGVzdEB0ZXN0LmZyIn0.ep9CgKrq5ai1M84Bhk8Vh_ygOTsiz67EvnJroqrus9hPWCjRV_qesY1ZXnQI1JT8yuV1wO0aJfAKxW1VtmOmwq7lQyw0gMpxBImoVZgrUSaCYU8Iwln_0YqBVuI0FmtztkBKZ2LMCiz8yThXrConQdt1mKsRL00hn_6_a1erpjwvcUOWWF5hwhbYsGemHfGmdAh_Cx1K3K_M9iPVvkb3rvWaGmid9YxM3OFoint9Kg4fyVGTuQu7O4FpRM5HbvxZ65LG_zb69H_GbCWtr_McCVkzD95-4H5wZuvgtly1QWRy7xDV-1tXanaoqbEYuf1SKCnWbEYJW3WbTORqhnLaFQ";
+
+export const getProduct = async (id: number | string) => {
+	const res: Response = await fetch(`${API_URL}/${id}`, {
+		headers: {
+			Authorization: ApiAuthToken,
+		},
+	});
+	if (res.ok) {
+		return await res.json();
+	} else {
+		throw new Error("Error on getProduct");
+	}
+};
 
 export const getProducts = async (queryParams?: URLSearchParams): Promise<HydraCollectionInterface> => {
-	const res: Response = await fetch(`${API_URL}${queryParams ? `?${queryParams}` : ""}`);
+	const res: Response = await fetch(`${API_URL}${queryParams ? `?${queryParams}` : ""}`, {
+		headers: {
+			Authorization: ApiAuthToken,
+		},
+	});
 	if (res.ok) {
 		return await res.json();
 	} else {
@@ -18,6 +37,7 @@ export const newProduct = async (body: {}) => {
 		body: values,
 		headers: {
 			"Content-Type": "application/json",
+			Authorization: ApiAuthToken,
 		},
 	});
 	if (!res.ok) {
@@ -25,13 +45,14 @@ export const newProduct = async (body: {}) => {
 	}
 };
 
-export const editProduct = async (body: { id: number }) => {
+export const editProduct = async (body: {}, id: number | string) => {
 	const values = JSON.stringify(body);
-	const res: Response = await fetch(`${API_URL}${`/${body.id}`}`, {
+	const res: Response = await fetch(`${API_URL}${`/${id}`}`, {
 		method: "PATCH",
 		body: values,
 		headers: {
-			"Content-Type": "application/json",
+			"Content-Type": "application/merge-patch+json",
+			Authorization: ApiAuthToken,
 		},
 	});
 	if (!res.ok) {
