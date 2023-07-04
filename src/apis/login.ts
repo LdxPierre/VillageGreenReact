3b18fb27fcc5695847cc254b3e74f6829d750049
Ormide <pierre.ledoux@outlook.fr>
2023-07-04 20:11:15 +0200

    denied access if not logged

--- src/apis/login.ts
+++ src/apis/login.ts
@@ -1,14 +1,25 @@
+import { getCookie } from "../services/utils";
+
 const API_URL = "http://localhost:8000/api/login";
 export const loginUser = async (values: { email: string; password: string }) => {
-	const body = JSON.stringify(values);
-	const res: Response = await fetch(API_URL, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body,
-	});
-	const resBody = await res.json();
-	const { status } = res;
-	return { status, resBody };
+  const body = JSON.stringify(values);
+  const res: Response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body,
+  });
+  const resBody = await res.json();
+  const { status } = res;
+  return { status, resBody };
+};
+
+export const loginCheck = async () => {
+  const res: Response = await fetch(API_URL, {
+    headers: {
+      authorization: `Bearer ${getCookie("token")}`,
+    },
+  });
+  return res;
 };
--- src/services/loaders/mainLoader.ts
+++ src/services/loaders/mainLoader.ts
@@ -1,3 +1,17 @@
-export const mainLoader = () => {
-	return null;
+import { redirect } from "react-router-dom";
+import { loginCheck } from "../../apis";
+
+export const mainLoader = async () => {
+  try {
+    const res: Response = await loginCheck();
+    if (res.status === 200) {
+      return await res.json();
+    } else if (res.status === 401) {
+      return redirect("/login");
+    } else {
+      throw new Error("Error on login check");
+    }
+  } catch (e) {
+    return e;
+  }
 };
